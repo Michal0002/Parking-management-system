faabbbdd787a3ba9d6e8b469a72c0add7f535124
Michał Kasperek <michal.kasperek2000@wp.pl>
2023-01-22 17:08:47 +0100

    Main page view, css (just temp.), links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'home/worker'
-  get 'home/client'
+  root 'home#index'
+  get 'home/worker', as: "worker"
+  get 'home/client', as: "client"
   get 'home/reservation'
   get 'home/contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
